d417df1158ce74bd4f238b57a33272c99e411597
martin <martinledrut@gmail.com>
2021-04-10 11:25:57 +0200

    Update

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 var trajVar = document.querySelector( '.trajVar' );
 var ressourceVar = document.querySelector( '.ressourcejVar' );
 var protecteurVar = document.querySelector( '.protecteurVar' );
+var rareVar = document.querySelector( '.rareVar' );
+let inputRessourceName = document.querySelector('#ressourceNameInput')
+let ressourceNameDOM = document.querySelector('#ressourceName')
+let inputRareName = document.querySelector('#rareNameInput')
+let rareNameDOM = document.querySelector('#rareName')
 
 
 var trajCounter = 0;
 var ressourceCounter = 0;
 var protecteurCounter = 0;
 var rareCounter = 0;
+
+var ressourceName = 'Frostiz';
+var rareName = 'Frostiz Soufflé';
 
 
 
@@ -16,8 +24,10 @@ var addTraj = function(){
 }
 
 var remTraj = function(){
+    if(trajCounter >=1 ){
     trajCounter--;
     render();
+    }
 }
 
 
@@ -28,12 +38,14 @@ var addRessource = function(){
 }
 
 var remRessource = function(){
+    if(ressourceCounter >=1){
     ressourceCounter--;
     render();
+    }
 }
 
 
-
+// PROTECTEUR //
 var addProtecteur = function(){
     protecteurCounter++;
     ressourceCounter = ressourceCounter + 50;
@@ -41,9 +53,35 @@ var addProtecteur = function(){
 }
 
 var remProtecteur = function(){
+    if(protecteurCounter >=1 ){
     protecteurCounter--;
     ressourceCounter = ressourceCounter - 50;
     render();
+    }
+}
+
+// RARE //
+var addRare = function(){
+    rareCounter++;
+    render();
+}
+
+var remRare = function(){
+
+if(rareCounter >=1 ){
+    rareCounter--;
+    render();
+    }
+}
+
+//NAME //
+var updateRessourceName = function(){
+    ressourceName = inputRessourceName.value;
+    ressourceNameDOM.innerText = inputRessourceName.value;
+}
+var updateRareName = function(){
+    rareName = inputRareName.value;
+    rareNameDOM.innerText = inputRareName.value;
 }
 
 
@@ -53,6 +91,7 @@ var render = function(){
     trajVar.innerHTML = trajCounter;
     ressourceVar.innerHTML = ressourceCounter;
     protecteurVar.innerHTML = protecteurCounter;
+    rareVar.innerHTML = rareCounter;
 
 }
 
